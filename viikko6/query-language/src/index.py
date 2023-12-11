06a55f55eac686bae48e5a2c95cd94d0341f5d62
SaijaGit <saija.piira@gmail.com>
2023-12-11 22:02:16 +0200

    vk. 6 t. 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All , Not, HasFewerThan 
+from matchers import And, HasAtLeast, PlaysIn, All , Not, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -16,13 +16,16 @@ def main():
     for player in stats.matches(matcher):
         print(player)
 
-    
+    print("\n")
+
     matcher = And(
         Not(HasAtLeast(2, "goals")),
         PlaysIn("NYR")
     )
     for player in stats.matches(matcher):
         print(player)
+
+    print("\n")
 
     matcher = And(
         HasFewerThan(2, "goals"),
@@ -31,9 +34,34 @@ def main():
     for player in stats.matches(matcher):
         print(player)
 
+    print("\n")
+
     filtered_with_all = stats.matches(All())
     print(len(filtered_with_all))
 
+    print("\n")
+
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("\n")
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+    for player in stats.matches(matcher):
+        print(player)
+    
+    print("\n")
 
 if __name__ == "__main__":
     main()
